e8db4ab98c3e70f45ae7ba562842927a9ec943dc
Kalle Lilja <desethsc@gmail.com>
2017-10-28 18:12:30 +0200

    jQuery removed

--- script.js
+++ script.js
@@ -1,67 +1,118 @@
-$(function() {
+window.onload = function() {
+    
+    // Variables
+    var request = new XMLHttpRequest(),
+        body = document.querySelector('body'),
+        localBtn = document.querySelector('.local-btn'),
+        publicBtn = document.querySelector('.public-btn'),
+        btn  = document.querySelectorAll('.btn'),
+        localIpv4Info = document.querySelector('.local-ipv4-info'),
+        localIp = document.querySelector('.local-ip'),
+        localCopy = document.querySelector('.local-copy'),
+        publicIpInfo = document.querySelector('.public-ip-info'),
+        publicIp = document.querySelector('.public-ip'),
+        publicCopy = document.querySelector('.public-copy'),
+        ua = navigator.userAgent.toLowerCase();
 
     // Localization
     if(navigator.language == 'sv' || navigator.language == 'sv-SE') {
         // Swedish
-        $('.local-ipv4-info').html('Din lokala IP address är');
-        $('.public-ip-info').html('Din publika IP address är');
+        localIpv4Info.innerHTML = "Din lokala IP address är";
+        publicIpInfo.innerHTML = "Din publika IP address är";
         // Display helpful message if WebRTC won't work
-        $('.local-ip').html('Endast tillgängligt i Chrome eller Firefox, sorry!');
-        $('.copy-btn-txt').html('Kopiera till urklipp');
+        localIp.innerHTML = "Endast tillgängligt i Chrome eller Firefox, sorry!";
+        Array.prototype.forEach.call(btn, function(el, i){
+            el.value = "Kopiera till urklipp";
+        });
     } else {
         // Everything else
-        $('.local-ipv4-info').html('Your local IP address is');
-        $('.public-ip-info').html('Your public IP address is');
+        localIpv4Info.innerHTML = "Your local IP address is";
+        publicIpInfo.innerHTML = "Your public IP address is";
         // Display helpful message if WebRTC won't work
-        $('.local-ip').html("Only available in Chrome or Firefox, sorry!");
-        $('.copy-btn-txt').html('Copy to Clipboard');
+        localIp.innerHTML = "Only available in Chrome or Firefox, sorry!";
+        Array.prototype.forEach.call(btn, function(el, i){
+            el.value = "Copy to Clipboard";
+        });
     }
 
-    // Internet Explorer / Edge preferences
+    // Internet Explorer / Edge settings
     function isIE () {
         // Check if IE/Edge using 'console.log(navigator.userAgent.toLowerCase())'
-        var myNav = navigator.userAgent.toLowerCase();
-        return (myNav.indexOf('msie') != -1 || myNav.indexOf('trident') != -1 || myNav.indexOf('edge') != -1) ? true : false;
+        return (ua.indexOf('msie') != -1 || ua.indexOf('trident') != -1 || ua.indexOf('edge') != -1) ? true : false;
     }
 
     if(isIE()) {
         // Hide the Copy to Clipboard buttons as they are not supported in IE.
-        $('.local-btn, .public-btn').css('visibility', 'hidden');
-        $('body').addClass('ie');
+        localBtn.style.visibility = 'hidden';
+        publicBtn.style.visibility = 'hidden';
+        if (body.classList)
+            body.classList.add('ie');
+        else
+            body.ie += ' ' + ie;
+    } else {
+        // IP Calculation
+        window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+        var pc = new RTCPeerConnection({iceServers: []}), 
+            noop = function () {};
+
+        pc.createDataChannel("");
+        pc.createOffer(pc.setLocalDescription.bind(pc), noop);
+        pc.onicecandidate = function (ice) {
+            if (!ice || !ice.candidate || !ice.candidate.candidate) return;
+            var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+            pc.onicecandidate = noop;
+            localIp.innerHTML = myIP;
+        };
+
+        // Loaded event
+        setTimeout(function() {
+            if (body.classList)
+                body.classList.add('loaded');
+            else
+                body.loaded += ' ' + loaded;
+        }, 250);
     }
 
-    // IP Calculation
-    window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    var pc = new RTCPeerConnection({iceServers: []}), 
-        noop = function () {};
-
-    pc.createDataChannel("");
-    pc.createOffer(pc.setLocalDescription.bind(pc), noop);
-    pc.onicecandidate = function (ice) {
-        if (!ice || !ice.candidate || !ice.candidate.candidate) return;
-        var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
-        pc.onicecandidate = noop;
-        $('.local-ip').html(myIP);
-    };
-
-    // Click events
-    $('.local-btn, .local-ip').on('click', function() {
-        let val = $('.local-ip').html();
-        $('.local-copy').val(val).select();
-        document.activeElement.blur();
-        document.execCommand('copy');
-    });
-
-    // Click events
-    $('.public-btn, .public-ip').on('click', function() {
-        let val = $('.public-copy').val();
-        $('.public-copy').val(val).select();
-        document.activeElement.blur();
-        document.execCommand('copy');
-    });
-
-    setTimeout(function() {
-        $('body').addClass('loaded');
-    }, 250);
+    // Local PHP API
+    request.open('GET', 'getIP.php', true);
     
-});
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText);
+
+            publicIp.innerHTML = data[0].ip;
+            publicCopy.innerHTML = data[0].ip;
+
+        } else {
+            console.warn("Error on PHP API Request, getIP.php")
+        }
+    };
+    
+    request.onerror = function() {
+        console.warn("Error on PHP API Request, getIP.php")
+    };
+    
+    request.send();
+
+    // Click events
+    document.addEventListener('click', function (event) {
+
+        if ( event.target.classList.contains('local-btn') || event.target.classList.contains('local-ip') ) {
+            let val = localIp.innerHTML;
+            localCopy.value = val;
+            localCopy.select();
+            document.activeElement.blur();
+            document.execCommand('copy');
+        }
+
+        else if ( event.target.classList.contains('public-btn') || event.target.classList.contains('public-ip') ) {
+            let val = publicIp.innerHTML;
+            publicCopy.value = val;
+            publicCopy.select();
+            document.activeElement.blur();
+            document.execCommand('copy');
+        }
+
+    }, false);
+
+};
